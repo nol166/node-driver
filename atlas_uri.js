c207310433be2212a55d26eccebc1215fe06f23c
John McCambridge <jmccamb@gmail.com>
2022-07-22 12:12:20 -0500

    remove

--- atlas_uri.js
+++
@@ -1,3 +0,0 @@
-// TODO: Add your MongoDB Atlas URI string here. Refer to https://www.mongodb.com/docs/guides/atlas/connection-string/
-module.exports = uri =
-  "mongodb+srv://<username>:<password>@<your-cluster-url>/bank?retryWrites=true&w=majority"
